7eecaa865ea100cbcac94ea185d3d3ada00971d0
Ricardo Gomes <pb.ricardogomes@gmail.com>
2023-08-10 17:45:53 -0300

    Atualização 10/08/2023

---
+++ lib/src/services/http_manager.dart
@@ -0,0 +1,47 @@
+import 'package:dio/dio.dart';
+
+abstract class HttpMethods {
+  static const String post = 'POST';
+  static const String get = 'GET';
+  static const String put = 'PUT';
+  static const String patch = 'PATCH';
+  static const String delete = 'DELETE';
+}
+
+class HttpManager {
+  Future<Map> restRequest({
+    required String url,
+    required String method,
+    Map? headers,
+    Map? body,
+  }) async {
+    //Headers da requisição
+    final defaultHeaders = headers?.cast<String, String>() ?? {}
+      ..addAll({
+        'content-type': 'application/json',
+        'accept': 'application/json',
+        'X-Parse-Application-Id': 'wK7GcEjr2V4br5q5mlR1kybQ5dvxMFDX0qtE1d6Y',
+        'X-Parse-REST-API-Key': '2kahi62fkWePLWAwC7k8aMrtQkobogcgkruMxbeB',
+      });
+
+    Dio dio = Dio();
+    try {
+      Response response = await dio.request(
+        url,
+        options: Options(
+          headers: defaultHeaders,
+          method: method,
+        ),
+        data: body,
+      );
+      // retorno do resultado do server (servidor backend)
+      return response.data;
+    } on DioException catch (e) {
+      // Retorno do erro da requisição Dio
+      return e.response?.data ?? {};
+    } catch (e) {
+      // retorno de map vazio para erro generalizado
+      return {};
+    }
+  }
+}
